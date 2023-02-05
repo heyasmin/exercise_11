8139258861e2a0cd87f6639ffc055e60fe725c62
Eyasmin Basher <eyasminbasher@gmail.com>
2023-02-05 10:39:25 +0000

    added comments

--- ex11_starter.py
+++ ex11_starter.py
@@ -3,6 +3,8 @@
 Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,French,German'
 # hyphen times the length of the string, the commas are part of the string
 print("-"*len(Belgium))
-
+# this method needs 2 arguments, so the first is the comma to get rid of (belongs to the string), and the second is the one that will replace
+# string.replace(oldvalue, newvalue)
 print(Belgium.replace(',',':'))
+
 
