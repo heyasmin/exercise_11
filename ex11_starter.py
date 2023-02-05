695df4461a6f4a4945cb04738089dd6ef524b3c4
Eyasmin Basher <eyasminbasher@gmail.com>
2023-02-05 10:27:09 +0000

    added part a exercise 11

--- ex11_starter.py
+++ ex11_starter.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python
 
 Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,French,German'
+# hyphen times the length of the string, the commas are part of the string
+print("-"*len(Belgium))
+
 
