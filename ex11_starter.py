cf327ae87aa0fbea75d7e7fa02b7faaaed5b2008
Eyasmin Basher <eyasminbasher@gmail.com>
2023-02-05 10:49:44 +0000

    started part c exercise 11

--- ex11_starter.py
+++ ex11_starter.py
@@ -7,4 +7,10 @@ print("-"*len(Belgium))
 # string.replace(oldvalue, newvalue)
 print(Belgium.replace(',',':'))
 
+# this is the slicing method
+belgium_population = Belgium[8:16]
+print(belgium_population)
+brussels_population = Belgium[26:32]
+print(brussels_population)
+
 
