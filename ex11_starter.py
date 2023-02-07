da3f5819c36bb70260bfa79bc1f79c9f2bb5299e
Salma <salmaibrahim813@gmail.com>
2023-02-07 20:23:18 +0000

    added changessuggested by Victoria in tutor session

--- ex11_starter.py
+++ ex11_starter.py
@@ -1,25 +1,33 @@
 #!/usr/bin/python
 
 Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,French,German'
-
+# turned the belgium variable into a list to then find the index
+Belgium_list = Belgium.split(',')
 # hyphen multiply the length of the string, the commas are part of the string
 
 print("-"*len(Belgium))
 
-# this method needs 2 arguments, so the first is the comma to get rid of (belongs to the string), and the second is the one that will replace
+# this method needs 2 arguments, so the first is the comma to get rid of (belongs to the string),
+# and the second is the one that will replace
 
 # string.replace(oldvalue, newvalue)
 
-print(Belgium.replace(',',':'))
+print(Belgium.replace(',', ':'))
 
 # this is the slicing method, returning a certain string using a start index and an end index.
 
-# created two variables to obtain 2 different parts of the string.
+# belgium_population = int(Belgium[8:16])
+# print(belgium_population)
+#
+# brussels_population = int(Belgium[26:32])
+# print(brussels_population)
 
-belgium_population = int(Belgium[8:16])
+# created two variables to obtain 2 different parts of the string and using the index to find in the list.
+
+belgium_population = int(Belgium_list[1])
 print(belgium_population)
-
-brussels_population = int(Belgium[26:32])
+cd 
+brussels_population = int(Belgium_list[3])
 print(brussels_population)
 
 # had to add the int() to turn the string into an integer
